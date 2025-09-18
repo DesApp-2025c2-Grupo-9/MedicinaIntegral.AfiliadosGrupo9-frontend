b042fbe15c5ff7f0fe9fcc0e175ef88779e7ec11
nanoluixi <victorricardo.gonzalvezchara@estudiantes.unahur.edu.ar>
2025-09-18 20:09:15 +0000

    Refactor ReintegroCard component to include ColumnaPrincipal, UsuarioActual, and EstadoVersion1 for enhanced display and functionality. Add reintegro state management in PruebaCards page.

--- src/components/cards/cards/ReintegroCard.jsx
+++ src/components/cards/cards/ReintegroCard.jsx
@@ -1,9 +1,53 @@
-import React from 'react'
+import ColumnaPrincipal from "./cardComponents/ColumnaPrincipal"
+import UsuarioActual from "./cardComponents/UsuarioActual"
+import BotonEditar from "./cardComponents/BotonEditar"
+import BotonPapelera from './cardComponents/BotonPapelera'
+import BotonObservaciones from "./cardComponents/BotonObservaciones"
+import EstadoVersion1 from "./cardComponents/EstadoVersion1"
+function ReintegroCard(props) {
+  //Estilo de la card
+  
+  let cardStyle = `grid grid-cols-2 m-3 p-3 bg-white rounded-xl shadow-md border border-gray-200 w-md`
+  
+  
+  let reintegro = props.reintegro
+  
+  return (<div className={cardStyle}>
+    <ColumnaPrincipal
+      titulo = {reintegro.especialidad}
+      subtitulo = {reintegro.medico}
+      campo1 = {'Fecha de la prestación ' + formatFecha(reintegro.fecha)}
+      campo2 = {reintegro.lugar}
+      campo3 = {reintegro.valor}
+    />
+    <div className="grid grid-rows-4">
 
-function ReintegroCard() {
-  return (
-    <div>ReintegroCard</div>
+        <UsuarioActual />
+        <EstadoVersion1 estado = {reintegro.estado}/>
+        {reintegro.estado == 'Pendiente' ? (
+          <>
+            <BotonEditar posicion={3} />
+            <BotonPapelera posicion={4} />
+          </>
+        ) : <BotonObservaciones/>
+        }
+      </div>
+  </div>
   )
 }
 
 export default ReintegroCard
+
+function formatFecha(fecha) {
+  //Recibe una fecha tipo Date como parámetro
+  //Retorna un String para mostrar la fecha al usuario
+
+  const dia = String(fecha.getDate()).padStart(2, "0");
+  const mes = String(fecha.getMonth() + 1)
+  const anio = fecha.getFullYear();
+
+  const horas = String(fecha.getHours()).padStart(2, "0")
+  const minutos = String(fecha.getMinutes()).padStart(2, "0");
+
+  return `${dia}/${mes}/${anio}`
+}
--- src/pages/PruebaCards.jsx
+++ src/pages/PruebaCards.jsx
@@ -1,6 +1,6 @@
 import { AutorizacionCard, RecetaCard, ReintegroCard, TurnosCard } from '../components/cards'
 function PruebaCards() {
-    const turno =  {
+  const turno = {
     especialidad: "Oftalmología",
     profesional: "Merlina Addams",
     fecha: new Date("1969-12-31T17:00:00"),
@@ -8,55 +8,88 @@ function PruebaCards() {
     direccion: "Arias 2030, Castelar",
     telefono: "11 3475 9678"
   };
-    const recetaPendiente = {
-      medicamento : 'Loplac 50mg',
-      cantidad : '2 cajas',
-      presentacion : 'Pastillas',
-      detalleMedicamento : '30 unidades por caja',
-      estado : 'Pendiente'
-    }
-    const recetaAceptada = {
-      medicamento : 'Loplac 50mg',
-      cantidad : '2 cajas',
-      presentacion : 'Pastillas',
-      detalleMedicamento : '30 unidades por caja',
-      estado : 'Aceptada'
-    }
-    const recetaObservadaRechazada = {
-      medicamento : 'Loplac 50mg',
-      cantidad : '2 cajas',
-      presentacion : 'Pastillas',
-      detalleMedicamento : '30 unidades por caja',
-      estado : 'Observada'
-    }
+  const recetaPendiente = {
+    medicamento: 'Loplac 50mg',
+    cantidad: '2 cajas',
+    presentacion: 'Pastillas',
+    detalleMedicamento: '30 unidades por caja',
+    estado: 'Pendiente'
+  }
+  const recetaAceptada = {
+    medicamento: 'Loplac 50mg',
+    cantidad: '2 cajas',
+    presentacion: 'Pastillas',
+    detalleMedicamento: '30 unidades por caja',
+    estado: 'Aceptada'
+  }
+  const recetaObservadaRechazada = {
+    medicamento: 'Loplac 50mg',
+    cantidad: '2 cajas',
+    presentacion: 'Pastillas',
+    detalleMedicamento: '30 unidades por caja',
+    estado: 'Observada'
+  }
+  const reintegroPendiente ={
+    fecha: new Date("1969-12-31T17:00:00"),
+    integrante : 'Mary Jane',
+    medico : 'Merlina Addams',
+    especialidad : 'Oftalmologia',
+    lugar : 'Centro Medicina Integral',
+    valor : '$130.000,00',
+    formaDePago: 'Efectivo',
+    estado : 'Pendiente'
+  }
+  const reintegroObservadoRechazado ={
+    fecha: new Date("1969-12-31T17:00:00"),
+    integrante : 'Mary Jane',
+    medico : 'Merlina Addams',
+    especialidad : 'Oftalmologia',
+    lugar : 'Centro Medicina Integral',
+    valor : '$130.000,00',
+    formaDePago: 'Efectivo',
+    estado: 'Rechazado'
+  }
+  const reintegroAceptado ={
+    fecha: new Date("1969-12-31T17:00:00"),
+    integrante : 'Mary Jane',
+    medico : 'Merlina Addams',
+    especialidad : 'Oftalmologia',
+    lugar : 'Centro Medicina Integral',
+    valor : '$130.000,00',
+    formaDePago: 'Efectivo',
+    estado: 'Aceptado'
+  }
 
 
 
-    return (
-        <>
-        <div>
-          <h1>Turno Disponible</h1>
-          <TurnosCard turno={turno}></TurnosCard>
-        </div>
-        <div>
-          <h1>Turno propio</h1>
-          <TurnosCard turno={turno} paciente={true}></TurnosCard>
-        </div>
-        <div>
-          <h1>Receta aceptada</h1>
-          <RecetaCard receta={recetaAceptada}/>
-        </div>
-        <div>
-          <h1>Receta pendiente</h1>
-          <RecetaCard receta={recetaPendiente}/>
-        </div>
-        <div>
-          <h1>Receta rechazada/observada</h1>
-          <RecetaCard receta={recetaObservadaRechazada}/>
-        </div>
-            <ReintegroCard />
-        </>
-    )
+  return (
+    <div className='grid grid-cols-auto grid-rows-2'>
+      <div className='col-start-1'>
+        <h1>Turno "Buscar turnos"</h1>
+        <TurnosCard turno={turno}></TurnosCard>
+        <h1>Turno "Mis turnos"</h1>
+        <TurnosCard turno={turno} paciente={true}></TurnosCard>
+      </div>
+      <div className='col-start-2'>
+        <h1>Receta aceptada</h1>
+        <RecetaCard receta={recetaAceptada} />
+
+        <h1>Receta pendiente</h1>
+        <RecetaCard receta={recetaPendiente} />
+
+        <h1>Receta rechazada/observada</h1>
+        <RecetaCard receta={recetaObservadaRechazada} />
+      </div>
+      <div className='col-start-1 row-start-2'>
+        <h1>Reintegro Pendiente</h1>
+        <ReintegroCard reintegro={reintegroPendiente}/>
+        <h1>Reintegro rechazado/observado</h1>
+        <ReintegroCard reintegro={reintegroObservadoRechazado}/>
+        <h1>ReintegroAceptado</h1>
+        <ReintegroCard reintegro={reintegroAceptado}/>
+      </div>
+    </div>
+  )
 }
 
 export default PruebaCards
