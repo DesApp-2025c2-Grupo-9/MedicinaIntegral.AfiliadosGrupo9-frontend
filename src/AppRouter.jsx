f9b2d4fda6540a5cd6f535289914bbf577e65b4c
WalterAurelio <aureliogareca.dev@gmail.com>
2025-09-21 15:04:41 -0300

    Se corrigió un problema responsive del layout MainLayout. Se definieron las rutas para el registro e inicio de sesión.

--- src/AppRouter.jsx
+++ src/AppRouter.jsx
@@ -3,11 +3,20 @@ import Pruebas from "./pages/Pruebas";
 import MainLayout from './layout/MainLayout';
 import SectionTitle from './components/SectionTitle'
 import PruebaCards from "./pages/PruebaCards";
+import PreLayout from './layout/PreLayout';
 
 
 export function AppRouter() {
   return (
     <Routes>
+
+      {/* Rutas públicas */}
+      <Route element={<PreLayout />} >
+        <Route path='/register' element={<h1>Ruta /register</h1>} />
+        <Route path='/login' element={<h1>Ruta /login</h1>} />
+      </Route>
+
+      {/* Rutas protegidas */}
       <Route element={<MainLayout />}>
         <Route path='/' element={<SectionTitle text={"Inicio"}/>} /> 
         <Route path='/mi-cuenta' element={<h1>Mi cuenta.</h1>} />
@@ -17,8 +26,11 @@ export function AppRouter() {
         <Route path='/autorizaciones' element={<h1>Autorizaciones.</h1>} />
         <Route path='/cartilla-medica' element={<h1>Cartilla Médica.</h1>} />
       </Route>
+
+      {/* Catch all */}
+      <Route path='*' element={<h1>Not Found.</h1>} />
+
       <Route path='/pruebaCards' element={<PruebaCards/>} /> 
-      
       <Route path="/pruebas" element={<Pruebas />} />
 
     </Routes>
---
+++ src/layout/PreLayout.jsx
@@ -0,0 +1,11 @@
+import { Outlet } from 'react-router-dom'
+
+function PreLayout() {
+  return (
+    <>
+      <i>Implementar layout de las páginas de Registro e Inicio de sesión.</i>
+      <Outlet />
+    </>
+  )
+}
+export default PreLayout
