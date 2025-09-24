1d4605da55c750cc41f6b21ce36df39dc8b5e5ef
nanoluixi <victorricardo.gonzalvezchara@estudiantes.unahur.edu.ar>
2025-09-24 22:49:53 +0000

    Mi cuenta creado, funcionalidad para ver situación terapeutica implementada

--- src/components/cards/cards/AfiliadoCard.jsx
+++ src/components/cards/cards/AfiliadoCard.jsx
@@ -1,12 +1,15 @@
+import ModalSituacionTerapeutica from "../../ModalSituacionTerapeutica/ModalSituacionTerapeutica"
 import CampoInformacion from "./cardComponents/CampoInformacion"
 import ColumnaPrincipal from "./cardComponents/ColumnaPrincipal"
 import MarcoCard from "./cardComponents/MarcoCard"
 import TituloCard from "./cardComponents/TituloCard"
+import { useState } from "react"
+
 
 function AfiliadoCard(props) {
     //Si no se pasa un afiliado por parámetro, se debe cargar el usuario del contexto, ya que es el actual
     const afiliado = props.afiliado
-
+    const [situacionTerapeuticaOpen, setsituacionTerapeuticaOpen] = useState(false)
     const campos = [
         `Nro. Afiliado: ${afiliado.nroAfiliado}`,
         //Si es el afiliado 01 muestra el plan, de lo contrario no lo muestra
@@ -14,8 +17,12 @@ function AfiliadoCard(props) {
         `DNI: ${afiliado.dni}`,
         `Email: ${afiliado.email}`
     ]
-
-    return (
+    const situacionTerapeuticaTexto = (
+        afiliado.situacionTerapeutica.length > 0 ? (
+            afiliado.situacionTerapeutica.join(', ') + '.'
+        ) : ''
+    )
+    return (<>
         <MarcoCard>
             <ColumnaPrincipal>
                 <TituloCard>{afiliado.nombre}</TituloCard>
@@ -23,11 +30,22 @@ function AfiliadoCard(props) {
                     (textoCampo, index) =>
                         <CampoInformacion key={index}>{textoCampo}</CampoInformacion>
                 )}
-                <button className="text-xs text-blue-500 grid justify-items-start col-start-1" onClick={() => {alert(afiliado.situacionTerapeutica)}}>
+                <button className="text-xs text-blue-500 grid justify-items-start col-start-1" onClick={() => { setsituacionTerapeuticaOpen(!situacionTerapeuticaOpen) }}>
                     Situación terapeutica
                 </button>
             </ColumnaPrincipal>
         </MarcoCard>
+        {
+            situacionTerapeuticaOpen ? (
+                <ModalSituacionTerapeutica
+                    open={situacionTerapeuticaOpen}
+                    onClose={() => { setsituacionTerapeuticaOpen(!situacionTerapeuticaOpen) }}
+                    nombreUsuario={afiliado.nombre}
+                    diagnosticoTexto={situacionTerapeuticaTexto}
+                />
+            ) : (<></>)
+        }
+    </>
     )
 }
 export default AfiliadoCard;
--- src/pages/MiCuenta.jsx
+++ src/pages/MiCuenta.jsx
@@ -1,8 +1,62 @@
+import SectionTitle from "../components/SectionTitle"
+import Button from '../components/Button'
+import { AfiliadoCard } from "../components/cards"
 
 
 function MiCuenta() {
+  const afiliado = {
+    nroAfiliado: '1234567-01',
+    nombre: 'Jane Doe',
+    planMedico: 'Oro',
+    dni: '13123123',
+    email: 'alguien@example.com',
+    situacionTerapeutica: [
+      'Miopia', 'Astigmatismo'
+    ]
+  }
+  const grupoFamiliar = [
+    {
+      nroAfiliado: '1234567-02',
+      nombre: 'Clara Doe',
+      planMedico: 'Oro',
+      dni: '13123125',
+      email: 'alguien@example.com',
+      situacionTerapeutica: [
+        'Miopia'
+      ]
+    }, {
+      nroAfiliado: '1234567-03',
+      nombre: 'Juan Doe',
+      planMedico: 'Oro',
+      dni: '13123125',
+      email: 'alguien@example.com',
+      situacionTerapeutica: [
+      ]
+    }
+  ]
   return (
-    <div>MiCuenta</div>
+    <>
+      <div>
+        <SectionTitle>Mi cuenta</SectionTitle>
+        <AfiliadoCard afiliado={afiliado} />
+      </div>
+      <div>
+        <SectionTitle>Grupo familiar</SectionTitle>
+        <div className="grid sm:grid-cols-1 md:grid-cols-2 xl:grid-cols-3">
+          {//Cargar al grupo familiar
+            grupoFamiliar.map(
+              (afiliado) =>
+                <AfiliadoCard afiliado={afiliado} key={afiliado.nroAfiliado} />
+            )
+          }
+        </div>
+      </div>
+      <div>
+        <SectionTitle>CBUs Registrados</SectionTitle>
+        <Button>Registrar nuevo CBU</Button>
+      </div>
+
+    </>
   )
 }
 
