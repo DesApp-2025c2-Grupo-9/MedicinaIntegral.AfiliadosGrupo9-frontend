449a238b7d8457e7fac4239ef8cccc942cf1275e
SilvinaAlvarez2811 <silvinaalvarez2811@gmail.com>
2025-10-25 18:24:17 -0300

    Trabajo en SolicitarReceta - guardando cambios antes de actualizar dev

--- src/pages/Recetas/SolicitarReceta.jsx
+++ src/pages/Recetas/SolicitarReceta.jsx
@@ -1,36 +1,38 @@
-import Form from '../../components/Form';
-import Input from '../../components/Input';
-import Button from '../../components/Button';
-import InputContainer from '../../components/InputContainer';
-import Select from '../../components/Select';
-import { useForm } from 'react-hook-form';
-import { zodResolver } from '@hookform/resolvers/zod';
-import Swal from 'sweetalert2';
-import { useNavigate } from 'react-router-dom';
-import { useNewRecetaSchema } from '../../hooks/useNewRecetaSchema';
-import { useUserStore } from '../../store/userStore';
-import { useCreateReceta } from '../../services/recetasQueries';
+import Form from "../../components/Form";
+import Input from "../../components/Input";
+import Button from "../../components/Button";
+import InputContainer from "../../components/InputContainer";
+import Select from "../../components/Select";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import Swal from "sweetalert2";
+import { useNavigate } from "react-router-dom";
+import { useNewRecetaSchema } from "../../hooks/useNewRecetaSchema";
+import { useUserStore } from "../../store/userStore";
+import { useCreateReceta } from "../../services/recetasQueries";
 
 function SolicitarReceta() {
   // const solicitudSchema = recetaSchema;
-  const { user } = useUserStore(state => state);
-  const listaAfiliados = user.grupoFamiliar?.map(familiar => `${familiar.nombre} ${familiar.apellido}`);
+  const { user } = useUserStore((state) => state);
+  const listaAfiliados = user.grupoFamiliar?.map(
+    (familiar) => `${familiar.nombre} ${familiar.apellido}`
+  );
   const { recetaSchema } = useNewRecetaSchema({ listaAfiliados });
   const { mutateAsync } = useCreateReceta();
-
+  console.log("Usuario logueado:", JSON.stringify(user, null, 2));
   //Se inicia el form con el resolver de zod
   const {
     register,
     handleSubmit,
-    formState: { errors, isSubmitting }
+    formState: { errors, isSubmitting },
   } = useForm({
-    resolver: zodResolver(recetaSchema)
+    resolver: zodResolver(recetaSchema),
   });
   const navigate = useNavigate();
 
-  const onSubmit = formData => {
+  const onSubmit = (formData) => {
     Swal.fire({
-      title: 'Confirmacion de solicitud',
+      title: "Confirmacion de solicitud",
       html: `
             <p>Está a punto de solicitar una receta con los siguientes datos:</p>
             <br/>
@@ -38,20 +40,20 @@ function SolicitarReceta() {
             <p>Presentación: ${formData.presentacion} </p>
             <p>Cantidad: ${formData.cantidad}</p>
             `,
-      icon: 'question',
-      confirmButtonText: 'Aceptar',
+      icon: "question",
+      confirmButtonText: "Aceptar",
       showCancelButton: true,
-      width: '400px'
-    }).then(async result => {
+      width: "400px",
+    }).then(async (result) => {
       if (result.isConfirmed) {
         //Insertar tryCatch con lógica para crear la receta
         await mutateAsync(formData);
         Swal.fire({
-          title: 'Enviado',
+          title: "Enviado",
           text: 'Su receta se ha enviado correctamente, puede encontrarla en "Ver Recetas"',
-          icon: 'success'
+          icon: "success",
         });
-        navigate('/recetas/ver-recetas');
+        navigate("/recetas/ver-recetas");
       }
     });
   };
@@ -60,53 +62,53 @@ function SolicitarReceta() {
     <Form onSubmit={handleSubmit(onSubmit)}>
       {/*Dropdown afiliado - Modificar para que aparezca el nro de afiliado tambien*/}
       <Select
-        {...register('paraAfiliado')}
-        id='paraAfiliado'
-        label='Para afiliado:'
-        placeholder='Seleccionar afiliado'
+        {...register("paraAfiliado")}
+        id="paraAfiliado"
+        label="Para afiliado:"
+        placeholder="Seleccionar afiliado"
         options={listaAfiliados}
         errorMsg={errors.paraAfiliado?.message}
       />
       {/*Input medicamento - Input Cantidad */}
       <InputContainer>
         <Input
-          {...register('medicamento')}
-          label={'Medicamento'}
-          placeholder={'Ingresar Medicamento'}
+          {...register("medicamento")}
+          label={"Medicamento"}
+          placeholder={"Ingresar Medicamento"}
           maxLength={50}
           errorMsg={errors.medicamento?.message}
         />
         <Input
-          {...register('cantidad', { valueAsNumber: true })}
-          type='number'
+          {...register("cantidad", { valueAsNumber: true })}
+          type="number"
           min={1}
           maxLength={2}
-          label={'Cantidad'}
-          placeholder={'Ingrese cantidad'}
+          label={"Cantidad"}
+          placeholder={"Ingrese cantidad"}
           errorMsg={errors.cantidad?.message}
         />
       </InputContainer>
       {/*InputPresentación */}
       <Input
-        {...register('presentacion')}
-        label={'Presentación'}
+        {...register("presentacion")}
+        label={"Presentación"}
         maxLength={50}
-        placeholder={'Ingrese la presentación'}
+        placeholder={"Ingrese la presentación"}
         errorMsg={errors.presentacion?.message}
       />
 
       {/*Input observaciones*/}
       <Input
-        {...register('observaciones')}
-        label={'Observaciones'}
+        {...register("observaciones")}
+        label={"Observaciones"}
         errorMsg={errors.observaciones?.message}
         maxLength={100}
       />
       <Button
-        type='submit'
-        state={isSubmitting ? 'disabled' : 'active'}
+        type="submit"
+        state={isSubmitting ? "disabled" : "active"}
         disabled={isSubmitting}
-        className={'ml-auto'}
+        className={"ml-auto"}
       >
         Confirmar
       </Button>
