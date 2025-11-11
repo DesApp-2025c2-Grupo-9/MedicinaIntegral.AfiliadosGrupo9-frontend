027aa7f4fc02b7d6c50f242850497561d6177e04
WalterAurelio <aureliogareca.dev@gmail.com>
2025-11-10 23:48:02 -0300

    Se modificó los estilos del componente AvatarAfiliado para adaptarlo a mobile.

--- src/components/Separador.jsx
+++ src/components/Separador.jsx
@@ -1,6 +1,8 @@
-function Separador() {
+import { twMerge } from 'tailwind-merge'
+
+function Separador({ className }) {
   return (
-    <div className='h-[1px] self-stretch bg-gris-border'></div>
+    <div className={twMerge('h-[1px] self-stretch bg-gris-border', className)}></div>
   )
 }
 export default Separador
