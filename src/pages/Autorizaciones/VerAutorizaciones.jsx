f19ec1ce829c1642b270c68083a39c2893c7e583
WalterAurelio <aureliogareca.dev@gmail.com>
2025-11-12 15:55:51 -0300

    Se solucionó un problema con el filtro de estados de los trámites, en los que el filtro activo se mantenía en todos los diferentes tipos de trámites, provocando problemas en el renderizado de las cards.

--- src/pages/Autorizaciones/VerAutorizaciones.jsx
+++ src/pages/Autorizaciones/VerAutorizaciones.jsx
@@ -1,15 +1,15 @@
 import AutorizacionCard from '../../components/cards/cards/AutorizacionCard';
-import { useStateFilter } from '../../store/stateFilter';
 import FiltroEstados from '../../components/FiltroEstados';
 import { useGetAllAutorizaciones } from '../../services/autorizacionesQueries';
 import { useLocation, useNavigate } from 'react-router-dom';
 import { capitalize } from 'lodash';
 import { useUserStore } from '../../store/userStore';
 import TramitesSkeleton from '../../components/Skeletons/TramitesSkeleton';
+import { useState } from 'react';
 
 function VerAutorizaciones() {
+  const [estadoTramite, setEstadoTramite] = useState('Todos');
   const navigate = useNavigate();
-  const { state } = useStateFilter();
   const location = useLocation();
   const { user } = useUserStore(state => state);
 
@@ -25,10 +25,14 @@ function VerAutorizaciones() {
     return <p>Error: {JSON.stringify(error)}</p>;
   }
 
-  const autorizacionesFiltradas = autorizaciones?.filter(autorizacion => state.includes(capitalize(autorizacion.estado)) || state == 'Todos');
+  const autorizacionesFiltradas = autorizaciones?.filter(autorizacion => estadoTramite.includes(capitalize(autorizacion.estado)) || estadoTramite == 'Todos');
+
   return (
     <div className='flex flex-col items-end gap-3 relative'>
-      <FiltroEstados className='sm:absolute -top-9.5 mr-auto' />
+      <FiltroEstados
+        className='sm:absolute -top-9.5 mr-auto'
+        setEstadoTramite={setEstadoTramite}
+      />
       <div className='w-full grid grid-cols-[repeat(auto-fill,minmax(400px,1fr))] gap-3'>
         {autorizacionesFiltradas.map((autorizacion, idAutorizacion) => (
           <AutorizacionCard
