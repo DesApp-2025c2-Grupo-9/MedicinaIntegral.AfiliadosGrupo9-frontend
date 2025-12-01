78c9c399289528dd33b17bf00052888c7166ba04
SilvinaAlvarez2811 <silvinaalvarez2811@gmail.com>
2025-12-01 18:23:47 -0300

    se corrige el mensaje cuando no hay recetas que correspondan al filtro solicitado

--- src/pages/Recetas/VerRecetas.jsx
+++ src/pages/Recetas/VerRecetas.jsx
@@ -1,24 +1,25 @@
-import RecetaCard from '../../components/cards/cards/RecetaCard';
-import FiltroEstados from '../../components/FiltroEstados';
-import { useGetRecetas } from '../../services/recetasQueries';
-import { useNavigate, useLocation } from 'react-router-dom';
-import { useUserStore } from '../../store/userStore';
-import TramitesSkeleton from '../../components/Skeletons/TramitesSkeleton';
-import { useState } from 'react';
-import NoTramitesAvailable from '../NoTramitesAvailable';
-import NoTramitesEstado from '../NoTramitesEstado';
-import filtrarTramitePorFecha from '../../utils/filtrarTramitePorFecha';
-import { twMerge } from 'tailwind-merge';
-import clsx from 'clsx';
+import RecetaCard from "../../components/cards/cards/RecetaCard";
+import FiltroEstados from "../../components/FiltroEstados";
+import { useGetRecetas } from "../../services/recetasQueries";
+import { useNavigate, useLocation } from "react-router-dom";
+import { useUserStore } from "../../store/userStore";
+import TramitesSkeleton from "../../components/Skeletons/TramitesSkeleton";
+import { useState } from "react";
+import NoTramitesAvailable from "../NoTramitesAvailable";
+import NoTramitesEstado from "../NoTramitesEstado";
+import filtrarTramitePorFecha from "../../utils/filtrarTramitePorFecha";
+import { twMerge } from "tailwind-merge";
+import clsx from "clsx";
 
 function VerRecetas() {
-  const [estadoTramite, setEstadoTramite] = useState('Todos');
+  const [estadoTramite, setEstadoTramite] = useState("Todos");
   const navigate = useNavigate();
   const location = useLocation();
-  const { user } = useUserStore(state => state);
+  const { user } = useUserStore((state) => state);
   const { data, error, isLoading } = useGetRecetas(user.idAfiliado);
   const recetas = data?.data;
-  const recetasFiltradas = data && filtrarTramitePorFecha(data.data, estadoTramite);
+  const recetasFiltradas =
+    data && filtrarTramitePorFecha(data.data, estadoTramite);
 
   if (isLoading) return <TramitesSkeleton />;
 
@@ -30,33 +31,31 @@ function VerRecetas() {
   }
 
   return (
-    <div className={twMerge(clsx('flex flex-col relative gap-3', { 'items-end': recetas }))}>
+    <div
+      className={twMerge(
+        clsx("flex flex-col relative gap-3", { "items-end": recetas })
+      )}
+    >
       {recetas ? (
         <>
           <FiltroEstados
-            className='sm:absolute -top-9.5 mr-auto'
+            className="sm:absolute -top-9.5 mr-auto"
             setEstadoTramite={setEstadoTramite}
           />
           {recetasFiltradas.length >= 1 ? (
-            <div className='w-full grid grid-cols-[repeat(auto-fill,minmax(400px,1fr))] gap-3'>
+            <div className="w-full grid grid-cols-[repeat(auto-fill,minmax(400px,1fr))] gap-3">
               {recetasFiltradas.map((receta, idReceta) => (
-                <RecetaCard
-                  receta={receta}
-                  key={idReceta}
-                />
+                <RecetaCard receta={receta} key={idReceta} />
               ))}
             </div>
           ) : (
-            <NoTramitesEstado
-              tipoTramite='reintegros'
-              estado={estadoTramite}
-            />
+            <NoTramitesEstado tipoTramite="recetas" estado={estadoTramite} />
           )}
         </>
       ) : (
         <NoTramitesAvailable
-          tipoTramite='Receta'
-          path='/recetas/solicitar-receta'
+          tipoTramite="Receta"
+          path="/recetas/solicitar-receta"
         />
       )}
     </div>
